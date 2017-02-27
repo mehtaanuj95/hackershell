c572ac047fde07e032cd2544a129596822f1205e
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2017-02-28 03:07:10 +0530

    added capcha to index page in register section

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,31 @@
+function DrawCaptcha()
+{
+    
+    var arr = [];
+    for( var i = 0; i < 8; i++)
+    {
+        arr[i] = Math.random().toString(36).substr(2,1);
+    }
+    var code = arr[0] + ' ' + arr[1] + ' ' + ' ' + arr[2] + ' ' + arr[3] + ' ' + arr[4] + ' '+ arr[5] + ' ' + arr[6];
+    document.getElementById("txtCaptcha").value = code
+}
+function ValidCaptcha()
+{
+    var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+    var str2 = removeSpaces(document.getElementById('txtInput').value);
+    console.log(str1);
+    console.log(str2);
+    if (str1 == str2) return true;        
+    return false;
+    
+}
+function removeSpaces(string)
+{
+    return string.split(' ').join('');
+}
 function validate()
 {
+
 	//colsole.log("executed");
 	var name = document.f2.reg_username.value;
 	var password = document.f2.reg_password.value;
@@ -8,8 +34,12 @@ function validate()
 
 	var atposition=email.indexOf("@");  
 	var dotposition=email.lastIndexOf(".");  
-
-	if(password != retype)
+	var result_captcha = ValidCaptcha();
+	if(result_captcha == false)
+	{
+		alert("captcha dosen't match. Please retype it correctly");
+	}
+	else if(password != retype)
 	{  
 		alert("password must be same!");  
 		return false;  
